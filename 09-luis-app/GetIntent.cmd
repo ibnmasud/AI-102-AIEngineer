21e8d90300c88ca49a19e8f212400996ae7261ee
Al-Mahmud <ali_al_mahmud@outlook.com>
2021-11-16 12:17:32 -0800

    Lab 5

--- 09-luis-app/GetIntent.cmd
+++ 09-luis-app/GetIntent.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 rem Set values for your Language Understanding app
-set app_id=YOUR_APP_ID
-set endpoint=YOUR_ENDPOINT
-set key=YOUR_KEY
+set app_id=1b8adde8-0cdc-4628-958e-454188f823db
+set endpoint=https://labs-lu.cognitiveservices.azure.com/
+set key=6ad4a633a770458bb78755be126a8607
 
 rem Get parameter and encode spaces for URL
 set input=%1
